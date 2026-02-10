c66ace9c163750b1e95319655357ff1c785ce7cb
vtanu23bio-dot <vtanu23bio@student.mes.ac.in>
2026-02-11 01:20:45 +0530

    Create id_header.dart

---
+++ lib/widgets/id_header.dart
@@ -0,0 +1,15 @@
+dart import 
+  'package:flutter/material.dart'; 
+class IdHeader extends StatelessWidget { 
+  const IdHeader({super.key});
+                                        
+@override 
+  Widget build(BuildContext context) { 
+    return const Text( 
+      'IDENTITY CARD',
+      style: TextStyle( 
+        color: Colors.white70, letterSpacing: 1.2,
+      ), 
+    ); 
+  } 
+}
